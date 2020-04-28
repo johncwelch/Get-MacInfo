8e7ff9268eae61cb14ca5ddcde1784966182e4a0
John Welch <jwelch@blackbird.local>
2020-04-28 12:58:39 -0400

    added more CPU related info

--- Get-MacInfo.ps1
+++ Get-MacInfo.ps1
@@ -14,6 +14,9 @@ if (-Not $IsMacOS)
 #dot notation to retrieve values. 
 
 $macInfoHash = @{}
+
+#uname section============================
+
 #we're going to try to mirror the results of the windows version of Get-ComputerInfo as much as possible
 #first get the kernel version as maintDarwinVersion. This is initially a string
 $getMainDarwinVersion = Invoke-Expression -Command "/usr/bin/uname -v"
@@ -37,8 +40,12 @@ $tempString = $mainDarwinVersionArray[3]
 #since that will end with a colon, strip the last character off the temp string
 $mainDarwinKernelVersion = $tempString.Substring(0,$tempString.Length-1)
 
-#et voila. Now we add it into our hashTable
-$macInfoHash.Add("macOSDarwinVersion", $mainDarwinKernelVersion)
+
+#get CPU Architecture
+$macInfoCPUArch = Invoke-Expression -Command "/usr/bin/uname -m"
+
+#sw_ver section===============================================================
+
 
 #now, let's get some basic higher-level info
 $macInfoOSVersion = Invoke-Expression -Command "/usr/bin/sw_vers -productVersion"
@@ -49,6 +56,8 @@ $macInfoOSName = Invoke-Expression -Command "/usr/bin/sw_vers -productName"
 $macInfoHash.Add("macOSCurrentVersion", $macInfoOSVersion)
 $macInfoHash.Add("macOSCurrentBuildNumber", $macInfoOSBuildNumber)
 $macInfoHash.Add("macOSProductName", $macInfoOSName)
+
+#system_profiler section=========================================================
 
 #now, let's get our system_profiler info
 $macInfoSystemProfilerRaw = Invoke-Expression -Command "/usr/sbin/system_profiler SPHardwareDataType"
@@ -120,11 +129,20 @@ $macInfoL3CacheSize = $macInfoL3CacheSize.TrimStart()
 $macInfoRAMSize = $macInfoSystemProfilerArrayList[8].Split(":")[1]
 $macInfoRAMSize = $macInfoRAMSize.TrimStart()
 
+#sysctl section===============================================================================
+$macInfoCPUBrand = Invoke-Expression -Command "/usr/sbin/sysctl -n machdep.cpu.brand_string"
+
+#other section================================================================================
+$macInfoBootDevice = Invoke-Expression -Command "/usr/sbin/bless --info --getboot"
+
 #get the current language
 $macInfoEFILanguage = (Get-Culture).DisplayName
 
 
 #into the hashtable with you!
+$macInfoHash.Add("macOSDarwinVersion", $mainDarwinKernelVersion)
+$macInfoHash.Add("CPUArchitecture", $macInfoCPUArch)
+
 $macInfoHash.Add("EFIVersion", $macInfoEFIVersion)
 $macInfoHash.Add("SMCVersion", $macInfoSMCVersion)
 $macInfoHash.Add("HardwareSerialNumber", $macInfoHardwareSN)
@@ -139,6 +157,9 @@ $macInfoHash.Add("CPUCoreCount", $macInfoCPUCoreCount)
 $macInfoHash.Add("CPUL2CacheSize", $macInfoCPUL2CacheSize)
 $macInfoHash.Add("L3CacheSize", $macInfoL3CacheSize)
 $macInfoHash.Add("RAMAmount", $macInfoRAMSize)
+
+$macInfoHash.Add("CPUBrandString", $macInfoCPUBrand)
+$macInfoHash.Add("BootDevice", $macInfoBootDevice)
 
 
 $macInfoHash
